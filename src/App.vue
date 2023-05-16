0bee7a2828660ebca4b9cfd62411520c1a55b1e7
Rafashhe <cwb.rafaella@gmail.com>
2023-05-16 15:04:29 -0300

    primeira funcionalidade de filtragem

--- src/App.vue
+++ src/App.vue
@@ -19,48 +19,74 @@ export default {
       formData: {
         title: "",
         content: "",
-      }
+      },
+      search: "",
     };
   },
-  methods: {
-handleClick (event) {
- 
-  const now = new Date ()
+  computed: {
+filteredPosts () {
+  // se search estiver vazio, retorne a lista completa de posts
+  if (!this.search) return this.posts;
 
-  const dataDaPostagem = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
+  // lista filtrada é o nome genérico
+  // se tiver qualquer coisa em search, faz o filtro
+const listaFiltrada = [];
 
-this.posts.push({
-  title: this.formData.title,
-  content: this.formData.content,
-  datetime: dataDaPostagem,
-});
-
-this.formData = {
-        title: "",
-        content: "",
-};
-},
-
-handleInputChange (event) {
-  const { name, value } = event.target;
-  this.formData[name] = value;
-}
+for (const post of this.posts) {
+  if (post.title.includes(this.search)) {
+    listaFiltrada.push(post);
+    
+    // operadores lógicos
   }
 }
-  </script>
+  return listaFiltrada;
+
+},
+},
+
+  methods: {
+    handleClick(event) {
+      const now = new Date();
+
+      const dataDaPostagem = `${now.getDate()}/${
+        now.getMonth() + 1
+      }/${now.getFullYear()}`;
+
+      this.posts.push({
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      });
+
+      this.formData = {
+        title: "",
+        content: "",
+      };
+    },
+
+    handleInputChange(event) {
+      const { name, value } = event.target;
+      this.formData[name] = value;
+    },
+  },
+};
+</script>
 
 <template>
+  
+  <input v-model="search" placeholder="Procure pelo título do post..." />
+
   <div id="lista-posts">
-    <div class="x" v-for="x in posts" :key="x.key">
-      <h3>{{ x.title }}</h3>
-      <h4>{{ x.datetime }}</h4>
-      <p>{{ x.content }}</p>
+    <div class="post" v-for="post in filteredPosts" :key="post.key">
+      <h3>{{ post.title }}</h3>
+      <h4>{{ post.datetime }}</h4>
+      <p>{{ post.content }}</p>
     </div>
   </div>
 
   <form>
     <input v-model="formData.title" placeholder="Título" />
-    
+
     <textarea
       name="content"
       :value="formData.content"

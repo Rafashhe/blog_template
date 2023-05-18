5d3efd8f1476f4ff9b0c6f92b7322649500674be
Rafashhe <cwb.rafaella@gmail.com>
2023-05-18 16:33:45 -0300

    organiza informações entre componente e views de criar e editar

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,10 +1,24 @@
 <script>
-import { RouterLink, RouterView } from "vue-router";
-import posts from '../App.vue'
+import PostForm from '../componentes/PostForm.vue';
+
+export default {
+    props: {
+        posts: Array,
+    },
+    data() {
+        return {
+            post: this.posts [this.$route.params.id],
+        };
+    },
+    components: {
+        PostForm,
+    },
+};
+
 </script>
-
+<!-- o "this" eu só coloco no script-->
 <template>
+   
+<PostForm :post="post" />
 
-
-<RouterView />
 </template>

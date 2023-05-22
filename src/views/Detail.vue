70b2cbe20eddff4abd85b36dc2f0b31c4be44cd7
Rafashhe <cwb.rafaella@gmail.com>
2023-05-22 14:55:15 -0300

    cria view de detalhamento

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,10 +1,37 @@
 <script>
-import { RouterLink, RouterView } from "vue-router";
-import posts from '../App.vue'
+export default {
+  props: {
+    posts: Array,
+  },
+  data() {
+    // acessa o parâmetro id uma única vez
+    const id = this.$route.params.id;
+
+    return {
+        // expondo a variável id para ser acessível no template
+      id: id,
+      post: this.posts[id],
+    };
+  },
+};
 </script>
-
+<!-- o "this" eu só coloco no script-->
 <template>
+    <h2>
+        {{ post.title }}
+    </h2>
+<h4>{{ post.datetime }}</h4>
+    <textarea readonly :value="post.content"></textarea>
 
-
-<RouterView />
 </template>
+
+<style scoped>
+
+textarea {
+    resize: none;
+    outline: none;
+    border: none;
+    width: 80%;
+}
+
+</style>
